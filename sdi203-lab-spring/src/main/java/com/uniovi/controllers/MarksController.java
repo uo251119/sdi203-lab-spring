e46c8460501032baf311d8482e2c3bda531f433f
UO251119 <UO251119@uniovi.es>
2019-02-25 09:15:25 +0100

    SDI-SpringBoot-Sesión-4.1-Sesiones

--- sdi203-lab-spring/src/main/java/com/uniovi/controllers/MarksController.java
+++ sdi203-lab-spring/src/main/java/com/uniovi/controllers/MarksController.java
@@ -1,5 +1,7 @@
 package com.uniovi.controllers;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +16,9 @@ import com.uniovi.services.UsersService;
 
 @Controller
 public class MarksController {
+
+	@Autowired
+	private HttpSession httpSession;
 
 	@Autowired
 	private MarksService marksService;
