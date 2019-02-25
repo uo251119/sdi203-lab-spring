a1aa366bb5d02c5a7c004a350879c01f54db4a07
UO251119 <UO251119@uniovi.es>
2019-02-25 09:31:06 +0100

    SDI-SpringBoot-Sesión-4.2-Roles

--- sdi203-lab-spring/src/main/java/com/uniovi/entities/User.java
+++ sdi203-lab-spring/src/main/java/com/uniovi/entities/User.java
@@ -90,4 +90,12 @@ public class User {
 		this.passwordConfirm = passwordConfirm;
 	}
 
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
 }
