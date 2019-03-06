48f5700aa95e1d68f757e98047c57bdd2f5d26e5
UO251119 <uo251119@uniovi.es>
2019-03-06 10:50:59 +0100

    SDI-Selenium-5.1-ClasePrincipal

---
+++ sdi203-lab-spring/src/test/java/com/uniovi/tests/NotaneitorTests.java
@@ -0,0 +1,51 @@
+package com.uniovi.tests;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class NotaneitorTests {
+	static String PathFirefox64 = "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe";
+	static String Geckdriver022 = "C:\\Users\\PcCom\\Desktop\\Uni 18-19\\SDI\\PL-SDI-Sesión5-material\\PL-SDI-Sesión5-material\\geckodriver024win64.exe";
+
+	static WebDriver driver = getDriver(PathFirefox64, Geckdriver022);
+
+	public static WebDriver getDriver(String PathFirefox, String Geckdriver) {
+		System.setProperty("webdriver.firefox.bin", PathFirefox);
+		System.setProperty("webdriver.gecko.driver", Geckdriver);
+		WebDriver driver = new FirefoxDriver();
+		return driver;
+	}
+
+	static String URL = "http://localhost:8090";
+
+	// Antes de cada prueba se navega al URL home de la aplicaciónn
+	@Before
+	public void setUp() {
+		driver.navigate().to(URL);
+	}
+
+	// Después de cada prueba se borran las cookies del navegador
+	@After
+	public void tearDown() {
+		driver.manage().deleteAllCookies();
+	}
+
+	// Antes de la primera prueba
+	@BeforeClass
+	static public void begin() {
+	}
+
+	// Al finalizar la última prueba
+	@AfterClass
+	static public void end() {
+		// Cerramos el navegador al finalizar las pruebas
+		driver.quit();
+	}
+}
